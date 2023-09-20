325a1970e6796cca99fb0341b3201b442359081d
CN-Curriculum <138328232+CN-Curriculum@users.noreply.github.com>
2023-09-20 18:30:35 -0400

    Add coding practice

---
+++ domManipulation.js
@@ -0,0 +1,40 @@
+// html elements
+let background = document.querySelector(".background");
+let pink = document.querySelector(".pink");
+let green = document.querySelector(".green");
+let blue = document.querySelector(".blue");
+let lightGreen = document.querySelector(".light-green");
+let row1 = document.querySelector(".row-1");
+
+// CHALLENGE #1 - make the rows match
+
+// 1. Set the syling of `background` to a background color of black.
+
+// 2. Set the styling of `pink` to a background color of pink.
+
+// 3. Set the styling of `green` to a background color of green.
+
+// 4. Set the styling of `blue` to a background color of blue.
+
+// 5. Set the styling of `lightGreen` to a background color of lightGreen.
+
+// 6. Set the styling of `row1` to have a font color of white.
+
+
+
+// CHALLENGE #2 - style the image
+
+let image = document.querySelector(".image");
+let imageButton = document.querySelector(".image-button");
+                                   
+// 7. Add an event listener to `imageButton`
+// - The event type should be "click" or "dblclick"
+
+// Include the next tasks in the body of the function.
+
+// 8. The image is wayyyy too big and needs a border.
+// - Set the width of `image` to 300px
+// - Set the border of `image` so that it's:
+//   - solid
+//   - hex color #42068c
+//   - 5 pixels
