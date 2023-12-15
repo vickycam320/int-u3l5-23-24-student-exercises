d1694f9027493102733a23af925050d0363fd8ae
vickycam320 <143820193+vickycam320@users.noreply.github.com>
2023-12-15 15:49:01 +0000

    completed all tasks

--- domManipulation.js
+++ domManipulation.js
@@ -9,17 +9,19 @@ let row1 = document.querySelector(".row-1");
 // CHALLENGE #1 - make the rows match
 
 // 1. Set the styling of `example1` to a background color of black.
+example1.style.backgroundColor = "black";
 
 // 2. Set the styling of `pink` to a background color of pink.
+pink.style.backgroundColor = "pink";
 
 // 3. Set the styling of `green` to a background color of green.
-
+green.style.backgroundColor = "green";
 // 4. Set the styling of `blue` to a background color of blue.
-
+blue.style.backgroundColor = "blue";
 // 5. Set the styling of `lightGreen` to a background color of lightGreen.
-
+lightGreen.style.backgroundColor = "lightGreen";
 // 6. Set the styling of `row1` to have a font color of white.
-
+row1.style.fontColor = "white";
 
 
 // CHALLENGE #2 - style the image
@@ -29,7 +31,12 @@ let imageButton = document.querySelector(".image-button");
                                    
 // 7. Add an event listener to `imageButton`
 // - The event type should be "click" or "dblclick"
-
+imageButton.addEventListener("click",function() {
+  image.style.width = "300px"
+  image.style.border = "solid"
+  image.style.borderColor = "#42068c"
+  image.style.borderWidth = "5px"
+})
 // Include the next tasks in the body of the function.
 
 // 8. The image is wayyyy too big and needs a border.
--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let button = document.querySelector("button");
 let img = document.querySelector("img");
 
 // 1. Press the button on the webpage to change it up! Take notice of what happens.
-// - What do you think .style does?
+// - What do you think .style does? it changes the style of the element
 
 // 2. Try the following:
 // - Make it so that the colors are different than what's used.
